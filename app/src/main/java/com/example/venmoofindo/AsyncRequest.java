473e9b879a018e2c9838bc5aff3446329aad796c
andreamuljono <andreamuljono@gmail.com>
2020-08-14 14:07:34 -0700

    import

---
+++ app/src/main/java/com/example/venmoofindo/AsyncRequest.java
@@ -0,0 +1,21 @@
+package com.example.venmoofindo;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+import javax.net.ssl.HttpsURLConnection;
+
+public class AsyncRequest extends Thread {
+}
--- app/src/main/java/com/example/venmoofindo/MainActivity.java
+++ app/src/main/java/com/example/venmoofindo/MainActivity.java
@@ -2,7 +2,20 @@ package com.example.venmoofindo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.net.CookieStore;
+import java.net.HttpCookie;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
 

7c0682341185e0bd3e1e881a55c781cda0123f05
andreamuljono <andreamuljono@gmail.com>
2020-08-15 02:36:28 -0700

    menu

--- app/src/main/java/com/example/venmoofindo/MainUI.java
+++ app/src/main/java/com/example/venmoofindo/MainUI.java
@@ -1,5 +1,6 @@
 package com.example.venmoofindo;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,18 +8,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainUI extends AppCompatActivity {
 
     private Button send_money_button;
     private Button request_money_button;
     private TextView greeting;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.app_bar_main);
 
         greeting = (TextView)findViewById(R.id.greeting);
 
@@ -36,6 +42,30 @@ public class MainUI extends AppCompatActivity {
                 requestMoneyUI();
             }
         });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navgiation_drawer_close);
+        if(drawer != null) {
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+        }
+
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
     private void sendMoneyUI() {
@@ -48,10 +78,10 @@ public class MainUI extends AppCompatActivity {
         Intent intent = new Intent(this, RequestMoney.class);
         startActivity(intent);
     }
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.main_menu,menu);
-//        return true;
-//    }
+//@Override
+//public boolean onCreateOptionsMenu(Menu menu) {
+//    MenuInflater inflater = getMenuInflater();
+//    inflater.inflate(R.menu.main_menu,menu);
+//    return true;
+//}
 }

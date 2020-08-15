bb1aaf4af2f1ecd802ffcf4cc9597eeaa2392758
Kenneth <kenneth.anthony@ruslim.net>
2020-08-14 18:08:14 -0700

    send money ui

---
+++ app/src/main/java/com/example/venmoofindo/ListOfUsers.java
@@ -0,0 +1,107 @@
+package com.example.venmoofindo;
+
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.util.Log;
+import android.widget.AutoCompleteTextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ListOfUsers {
+
+    private RequestQueue requestQueue;
+    private String baseUrl = "https://api.github.com/users/";
+    private String url;
+    private ArrayList<String> users;
+
+    public ListOfUsers(Context context) {
+        requestQueue = Volley.newRequestQueue(context);
+        users = new ArrayList<>();
+    }
+
+    private void clearRepoList() {
+        // This will clear the repo list (set it as a blank string).
+        users.clear();
+    }
+
+    private void addToUserList(String repoName, String lastUpdated) {
+        // This will add a new repo to our list.
+        // It combines the repoName and lastUpdated strings together.
+        // And then adds them followed by a new line (\n\n make two new lines).
+        users.add(repoName);
+    }
+
+    private void setUserListText(String str) {
+        // This is used for setting the text of our repo list box to a specific string.
+        // We will use this to write a "No repos found" message if the user doens't have any.
+        clearRepoList();
+        users.add(str);
+    }
+
+    private void getUserList(String username) {
+        // First, we insert the username into the repo url.
+        // The repo url is defined in GitHubs API docs (https://developer.github.com/v3/repos/).
+        this.url = this.baseUrl + username + "/repos";
+
+        // Next, we create a new JsonArrayRequest. This will use Volley to make a HTTP request
+        // that expects a JSON Array Response.
+        // To fully understand this, I'd recommend readng the office docs: https://developer.android.com/training/volley/index.html
+        JsonArrayRequest arrReq = new JsonArrayRequest(Request.Method.GET, url,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        // Check the length of our response (to see if the user has any repos)
+                        if (response.length() > 0) {
+                            // The user does have repos, so let's loop through them all.
+                            for (int i = 0; i < response.length(); i++) {
+                                try {
+                                    // For each repo, add a new line to our repo list.
+                                    JSONObject jsonObj = response.getJSONObject(i);
+                                    String repoName = jsonObj.get("name").toString().trim();
+                                    String lastUpdated = jsonObj.get("updated_at").toString();
+                                    addToUserList(repoName, lastUpdated);
+                                } catch (JSONException e) {
+                                    // If there is an error then output this to the logs.
+                                    Log.e("Volley", "Invalid JSON Object.");
+                                }
+
+                            }
+                        } else {
+                            // The user didn't have any repos.
+                            setUserListText("No repos found.");
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // If there a HTTP error then add a note to our repo list.
+                        setUserListText("Error while calling REST API");
+                        Log.e("Volley", error.toString());
+                    }
+                }
+        );
+        // Add the request we just defined to our request queue.
+        // The request queue will automatically handle the request as soon as it can.
+        requestQueue.add(arrReq);
+    }
+
+    public ArrayList<String> getUsers(String username){
+        getUserList(username);
+        return users;
+    }
+}
--- app/src/main/java/com/example/venmoofindo/SendMoney.java
+++ app/src/main/java/com/example/venmoofindo/SendMoney.java
@@ -7,10 +7,34 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
 
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.Response;
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
 public class SendMoney extends AppCompatActivity {
     private static final String[] COUNTRIES = new String[]{
             "Afghanistan", "Albania", "Algeria", "Andorra", "Angola"
     };
+
+    private ArrayList<String> users;
+    AutoCompleteTextView autoCompleteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,17 +42,28 @@ public class SendMoney extends AppCompatActivity {
         setContentView(R.layout.activity_send_money);
 
 
-        AutoCompleteTextView editText = findViewById(R.id.users);
-        ArrayAdapter<String> autoComepleteAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,  COUNTRIES);
-        editText.setAdapter(autoComepleteAdapter);
+        users = new ArrayList<>();
+        autoCompleteText = findViewById(R.id.users);
 
+
+        // initialize users
+        ListOfUsers listOfUsers = new ListOfUsers(this);
+        users = listOfUsers.getUsers("Xuzii");
+
+        ArrayAdapter<String> autoComepleteAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,  users);
+        autoCompleteText.setAdapter(autoComepleteAdapter);
+
+
+        // initialize banks
         Spinner spinner = (Spinner) findViewById(R.id.banks);
         ArrayAdapter<String> spinnerAdapter =
-                new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, COUNTRIES);
+                new ArrayAdapter<String>(getApplicationContext(),  android.R.layout.simple_spinner_dropdown_item, users);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner.setAdapter(spinnerAdapter);
 
     }
+
+
 }

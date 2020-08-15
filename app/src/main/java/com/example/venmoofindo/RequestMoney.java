2b4824d632c892fa3910b6244e65836c70a79ff0
andreamuljono <andreamuljono@gmail.com>
2020-08-14 20:24:25 -0700

    few edits

--- app/src/main/java/com/example/venmoofindo/RequestMoney.java
+++ app/src/main/java/com/example/venmoofindo/RequestMoney.java
@@ -1,4 +1,13 @@
 package com.example.venmoofindo;
 
-public class RequestMoney {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RequestMoney extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_money);
+    }
 }
---
+++ app/src/main/java/com/example/venmoofindo/SignUp.java
@@ -0,0 +1,19 @@
+package com.example.venmoofindo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_userpass_page);
+
+    }
+}

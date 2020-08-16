79aac6a420603f1cca85c8b7b3875174037597e3
andreamuljono <andreamuljono@gmail.com>
2020-08-15 17:35:02 -0700

    fixed sign up

--- app/src/main/java/com/example/venmoofindo/Success.java
+++ app/src/main/java/com/example/venmoofindo/Success.java
@@ -2,7 +2,11 @@ package com.example.venmoofindo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
 import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -11,6 +15,7 @@ import android.widget.ImageView;
 public class Success extends AppCompatActivity {
 
     private ImageView mImgCheck;
+    boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class Success extends AppCompatActivity {
         setContentView(R.layout.check);
 
         mImgCheck = (ImageView) findViewById(R.id.imageView);
-        ((Animatable) mImgCheck.getDrawable()).start();
+        Animatable animated = ((Animatable) mImgCheck.getDrawable());
+        animated.start();
     }
 }
